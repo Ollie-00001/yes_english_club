02b3559315a1e1290a9ea3ea7cb0f36f43464429
Ollie <Ollie_0@mail.ru>
2025-08-08 19:15:00 +0700

    wrote views for added urls

--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+def about(request):
+    return render(request, 'core/about.html')
+
+def contacts(request):
+    return render(request, 'core/contacts.html')
+
+def order_details(request):
+    return render(request, 'core/order_details.html')
+
+def orders(request):
+    return render(request, 'core/orders.html')
+
+def reviews(request):
+    return render(request, 'core/reviews.html')
+
+def services(request):
+    return render(request, 'core/services.html')
