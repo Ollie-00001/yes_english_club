a57925e70d998bd3a9ad5bada510405fc7599f8f
Ollie <Ollie_0@mail.ru>
2025-08-23 16:56:42 +0700

    registered review model in admin

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Request
+from .models import Request, Review
 
 @admin.register(Request)
 class RequestAdmin(admin.ModelAdmin):
     list_display = ['client_name', 'email', 'phone_number', 'created_at']
     search_fields = ['client_name', 'email', 'phone_number']
     list_filter = ['created_at']
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['client_name', 'text']
+    search_fields = ['client_name', 'text']
+
