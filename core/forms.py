86c1807815d973a115cfa0f585a75a96cb756b7b
Ollie <Ollie_0@mail.ru>
2025-08-23 15:59:44 +0700

    added forms.py and created form for request model

---
+++ core/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from phonenumber_field.formfields import PhoneNumberField as FormPhoneNumberField
+from phonenumber_field.phonenumber import to_python
+from .models import Request
+
+class RequestForm(forms.ModelForm):
+    phone_number = FormPhoneNumberField(region='RU')
+
+    class Meta:
+        model = Request
+        fields = ['client_name', 'email', 'phone_number', 'message']
+
+    def clean_phone_number(self):
+        number = self.cleaned_data['phone_number']
+        if str(number).startswith('8'):
+            number = to_python('+7' + str(number)[1:])
+        return number
