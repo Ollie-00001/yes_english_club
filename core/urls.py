49ddb43264a61746b97fdfad1637fb7b8f193e0b
Ollie <Ollie_0@mail.ru>
2025-08-23 13:14:36 +0700

    change requests route to the current one

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('services/', views.services, name='services'),
     path('contacts/', views.contacts, name='contacts'),
     path('reviews/', views.reviews, name='reviews'),
-    path('orders/', views.orders, name='orders'),
+    path('requests/', views.RequestView.as_view(), name='requests'),
     path('order_details/', views.order_details, name='order_details'),
 ]
