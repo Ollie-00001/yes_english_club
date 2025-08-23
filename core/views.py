3a0c70102099096fbc50d071c506406f689108fb
Ollie <Ollie_0@mail.ru>
2025-08-23 13:21:43 +0700

    replaced orders view with CBV Request view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+from django.views.generic import TemplateView, ListView, DetailView, CreateView
+from .models import Request
 from django.shortcuts import render
 
 def about(request):
@@ -9,8 +11,12 @@ def contacts(request):
 def order_details(request):
     return render(request, 'core/order_details.html')
 
-def orders(request):
-    return render(request, 'core/orders.html')
+class RequestView(TemplateView):
+    template_name = 'core/requests.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['requests'] = Request.objects.all()
+        return context
 
 def reviews(request):
     return render(request, 'core/reviews.html')
