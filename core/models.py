ae926ca3a14b34727f52a709a38d5514e85af59a
Ollie <Ollie_0@mail.ru>
2025-08-23 13:20:17 +0700

    add Request model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Request(models.Model):
+    client_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.client_name} ({self.created_at.strftime('%d.%m %H:%M')})"
