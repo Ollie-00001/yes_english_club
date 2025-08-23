60569f0e6b019eca5f28b85b31bfae886931858e
Ollie <Ollie_0@mail.ru>
2025-08-23 16:00:17 +0700

    fixed request model display in admin

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Request
 
-admin.site.register(Request)
+@admin.register(Request)
+class RequestAdmin(admin.ModelAdmin):
+    list_display = ['client_name', 'email', 'phone_number', 'created_at']
+    search_fields = ['client_name', 'email', 'phone_number']
+    list_filter = ['created_at']
