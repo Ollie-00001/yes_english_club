136122757b7d9538a701027ec5e07b1efb984469
Ollie <Ollie_0@mail.ru>
2025-08-30 14:55:47 +0700

    updated models by adding verbose name to all fields

--- core/models.py
+++ core/models.py
@@ -1,24 +1,46 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from phonenumber_field.modelfields import PhoneNumberField
 
 class Request(models.Model):
-    client_name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone_number = PhoneNumberField(region='RU', null=True, blank=False)
+    client_name = models.CharField(max_length=100, verbose_name='Имя клиента')
+    email = models.EmailField(verbose_name='Почта')
+    phone_number = PhoneNumberField(region='RU', null=True, blank=False, verbose_name='Номер телефона')
     message = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания заявки')
+
+    class Meta:
+        verbose_name = "Заявка"
+        verbose_name_plural = "Заявки"
 
     def __str__(self):
         return f"{self.client_name} ({self.created_at.strftime('%d.%m %H:%M')})"
     
 class Review(models.Model):
-    text = models.TextField(max_length=500)
-    client_name = models.CharField(max_length=100, blank=True)
+    client_name = models.CharField(max_length=100, blank=True, verbose_name='Имя клиента')
+    text = models.TextField(max_length=500, verbose_name='Текст отзыва')
+    rating = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)], 
+        verbose_name='Оценка',
+        default=5
+        )
+    is_published = models.BooleanField(default=True, verbose_name="Опубликован")
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+
+    def __str__(self):
+        return f'{self.client_name or 'Аноним'} - {self.rating}/5'
 
 class Service(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    title = models.CharField(max_length=100, verbose_name='Услуга')
+    description = models.TextField(verbose_name='Описание')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Стоимость')
+
+    class Meta:
+        verbose_name = 'Услуга'
+        verbose_name_plural = 'Услуги'
 
     def __str__(self):
         return self.title
