a6ced5727a80ae7698029777ab9fc25f5dee3740
Ollie <Ollie_0@mail.ru>
2025-09-02 12:26:29 +0700

    sorted list of views + added review create view and thanks for review

--- core/views.py
+++ core/views.py
@@ -1,6 +1,5 @@
-from django.views.generic import TemplateView, ListView, DetailView, CreateView
-from django.views.generic.edit import FormView
-from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, FormView
+from django.contrib import messages
 from django.urls import reverse_lazy
 from .forms import RequestForm
 from .models import Request, Review, Service
@@ -11,35 +10,45 @@ class AboutView(TemplateView):
 class ContactsView(FormView):
     template_name = 'core/contacts.html'
     form_class = RequestForm
-    success_url = '/thanks_for_request/'
+    success_url = reverse_lazy('thanks_for_request')
 
     def form_valid(self, form):
         form.save()
+        messages.success(self.request, 'Ваша заявка принята!')
         return super().form_valid(form)
-
-class RequestDetailsView(TemplateView):
-    template_name = 'core/request_details.html'
-
-class RequestView(TemplateView):
-    template_name = 'core/requests.html'
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['requests'] = Request.objects.all()
-        return context
-
-class ReviewsView(TemplateView):
-    template_name = 'core/reviews.html'
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['reviews'] = Review.objects.all()
-        return context
-
-class ServicesView(TemplateView):
-    template_name = 'core/services.html'
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['services'] = Service.objects.all()
-        return context
 
 class ThanksForRequestView(TemplateView):
     template_name = 'core/thanks_for_request.html'
+
+class RequestView(ListView):
+    model = Request
+    template_name = 'core/requests.html'
+    context_object_name = 'requests'
+
+class RequestDetailsView(DetailView):
+    model = Request
+    template_name = 'core/request_details.html'
+    context_object_name = 'request_obj'
+
+class ReviewsView(ListView):
+    model = Review
+    template_name = 'core/reviews.html'
+    context_object_name = 'reviews'
+
+class ReviewCreateView(CreateView):
+    model = Review
+    template_name = 'core/create_review.html'
+    fields = ['client_name', 'text', 'rating']
+    success_url = reverse_lazy('thanks_for_review')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Ваш отзыв был успешно отправлен!')
+        return super().form_valid(form)
+    
+class ThanksForReviewView(TemplateView):
+    template_name = 'core/thanks_for_review.html'
+
+class ServicesView(ListView):
+    model = Service
+    template_name = 'core/services.html'
+    context_object_name = 'services'
