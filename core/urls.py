e5887e96e300f8a211e662c40df869df2aca1fc0
Ollie <Ollie_0@mail.ru>
2025-09-02 12:27:11 +0700

    added thanks pages and create review routes to urls

--- core/urls.py
+++ core/urls.py
@@ -7,8 +7,10 @@ urlpatterns = [
     path('about/', views.AboutView.as_view(), name='about'),
     path('services/', views.ServicesView.as_view(), name='services'),
     path('contacts/', views.ContactsView.as_view(), name='contacts'),
+    path('thanks_for_request/', views.ThanksForRequestView.as_view(), name='thanks_for_request'),
     path('reviews/', views.ReviewsView.as_view(), name='reviews'),
+    path('reviews/create/', views.ReviewCreateView.as_view(), name='create_review'),
+    path('thanks_for_review/', views.ThanksForReviewView.as_view(), name='thanks_for_review'),
     path('requests/', views.RequestView.as_view(), name='requests'),
     path('order_details/', views.RequestDetailsView.as_view(), name='order_details'),
-    path('thanks_for_request/', views.ThanksForRequestView.as_view(), name='thanks'),
 ]
