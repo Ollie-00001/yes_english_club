9426b896c0969163b877c187beb824580cde0b77
Ollie <Ollie_0@mail.ru>
2025-09-02 10:54:33 +0700

    adjusted contacts form validation and redirect to thanks page

--- core/views.py
+++ core/views.py
@@ -1,12 +1,21 @@
 from django.views.generic import TemplateView, ListView, DetailView, CreateView
-from .models import Request, Review, Service
+from django.views.generic.edit import FormView
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from .forms import RequestForm
+from .models import Request, Review, Service
 
 class AboutView(TemplateView):
     template_name = 'core/about.html'
 
-class ContactsView(TemplateView):
+class ContactsView(FormView):
     template_name = 'core/contacts.html'
+    form_class = RequestForm
+    success_url = '/thanks_for_request/'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
 class RequestDetailsView(TemplateView):
     template_name = 'core/request_details.html'
@@ -31,3 +40,6 @@ class ServicesView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['services'] = Service.objects.all()
         return context
+
+class ThanksForRequestView(TemplateView):
+    template_name = 'core/thanks_for_request.html'
