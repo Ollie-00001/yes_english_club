dbaa417c2a86a8110b4377ba6bfcc04c0ea1d44f
Ollie <Ollie_0@mail.ru>
2025-09-06 12:21:09 +0700

    make separate navbar and icons display

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,13 +1,18 @@
 def menu_items(request):
-    menu = [
-        {'title': 'О себе', 'url': 'about'},
-        {'title': 'Услуги', 'url': 'services'},
-        {'title': 'Контакты', 'url': 'contacts'},
-        {'title': 'Отзывы', 'url': 'reviews'},
+    user_menu = [
+        {'title': 'О себе', 'url': 'about', 'icon': 'bi bi-person'},
+        {'title': 'Услуги', 'url': 'services', 'icon': 'bi bi-card-checklist'},
+        {'title': 'Контакты', 'url': 'contacts', 'icon': 'bi bi-phone'},
+        {'title': 'Отзывы', 'url': 'reviews', 'icon': 'bi bi-award'},
+        {'title': 'Расписание', 'url': 'schedule', 'icon': 'bi bi-calendar-week'},
     ]
 
+    admin_menu = []
     if request.user.is_authenticated and request.user.is_staff:
-        menu.append({'title': 'Заявки', 'url': 'requests'})
-        menu.append({'title': 'Админка', 'url': 'admin:index'})
+        admin_menu.append({'title': 'Заявки', 'url': 'requests', 'icon': 'bi bi-view-list'})
+        admin_menu.append({'title': 'Админка', 'url': 'admin:index', 'icon': 'bi bi-shield-lock'})
 
-    return {'menu_items': menu}
+    return {
+        'user_menu': user_menu,
+        'admin_menu': admin_menu,
+    }
