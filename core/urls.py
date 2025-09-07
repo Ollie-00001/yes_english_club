b9c410bed6dd8178cfba0e64b2f4dd43058af9d6
Ollie <Ollie_0@mail.ru>
2025-09-07 17:13:18 +0700

    add route for schedule view to urls

--- core/urls.py
+++ core/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('reviews/', views.ReviewsView.as_view(), name='reviews'),
     path('reviews/create/', views.ReviewCreateView.as_view(), name='create_review'),
     path('thanks_for_review/', views.ThanksForReviewView.as_view(), name='thanks_for_review'),
+    path('schedule/', views.ScheduleView.as_view(), name='schedule'),
     path('requests/', views.RequestView.as_view(), name='requests'),
     path('order_details/', views.RequestDetailsView.as_view(), name='order_details'),
 ]
