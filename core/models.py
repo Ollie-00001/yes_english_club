6c719596a770293e1fc882687f189923fba5c3da
Ollie <Ollie_0@mail.ru>
2025-09-16 12:48:35 +0700

    create model for carousel images

--- core/models.py
+++ core/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
 from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.html import format_html
 import re
-import requests
 
 class Request(models.Model):
     client_name = models.CharField(max_length=100, verbose_name='Имя клиента')
@@ -75,6 +73,19 @@ class Teacher(models.Model):
 
     def __str__(self):
         return self.name
+    
+class GalleryImage(models.Model):
+    title = models.CharField(max_length=255, blank=True)
+    image = models.ImageField(upload_to='gallery/')
+    order = models.PositiveIntegerField(default=0, db_index=True)
+
+    class Meta:
+        ordering = ['order']
+        verbose_name = 'Фото для карусели'
+        verbose_name_plural = 'Фотографии для карусели'
+
+    def __str__(self):
+        return self.title or f"Фото {self.pk}"
 
 class Logo(models.Model):
     image = models.ImageField(upload_to='logos/', verbose_name='Логотип')
