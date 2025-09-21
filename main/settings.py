8b82a78bbd396cf0a473af5dc57f4f64007758f1
Ollie <Ollie_0@mail.ru>
2025-09-21 12:45:21 +0700

    add teacher context to template options

--- main/settings.py
+++ main/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'core.context_processors.menu_items',
+                'core.context_processors.teacher_context',
                 'core.context_processors.logo_context',
             ],
         },
